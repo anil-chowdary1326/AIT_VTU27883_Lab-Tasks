90be3f66a4e260217897d38f99ff0230836928f0
anil-chowdary1326 <vtu27883@veltech.edu.in>
2026-08-27 09:58:12 +0530

    Task 2_hill climbing.py

--- Task 2_hill climbing.py
+++ Task 2_hill climbing.py
@@ -29,3 +29,36 @@ start = 3
 best_state = hill_climbing(start)
 
 print("Best State Found:", best_state)
+
+Output:
+Current State: 3
+Neighbors: [2, 4]
+Moving to: 4
+
+Current State: 4
+Neighbors: [3, 5]
+Moving to: 5
+
+Current State: 5
+Neighbors: [4, 6]
+Moving to: 6
+
+Current State: 6
+Neighbors: [5, 7]
+Moving to: 7
+
+Current State: 7
+Neighbors: [6, 8]
+Moving to: 8
+
+Current State: 8
+Neighbors: [7, 9]
+Moving to: 9
+
+Current State: 9
+Neighbors: [8, 10]
+Moving to: 10
+
+Current State: 10
+Neighbors: [9, 11]
+Best State Found: 10
